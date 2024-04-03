ddda4322fef7a430dd352990e44f4e0b2b313b1c
fox3u <fox3u@VREDINN>
2024-04-03 22:31:41 +0500

    Добавьте файлы проекта.

---
+++ CareerOrientationBot/CareerOrientationBot.py
@@ -0,0 +1,26 @@
+﻿import config
+import aiogram
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+bot = Bot(token=config.TOKEN)
+dp=Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Приветик!')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Выход')
+        
