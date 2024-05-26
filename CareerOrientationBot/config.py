ba5a2c15e90c4038171dcc9fe773c721a67edc42
Victor Dmitriev <fox3un@gmail.com>
2024-05-26 15:29:13 +0500

    config

---
+++ CareerOrientationBot/config.py
@@ -0,0 +1,5 @@
+TOKEN = '7032715627:AAFYhp2eF6skZSrKl36bsrYebxt4eeedvWU' #Токен ТГ бота
+host = '141.8.192.82'                                    #IP-адрес БД
+user = 'a0947295_admin'                                  #Имя пользователя
+password = 'killerbob'                                   #Пароль пользователя
+db = 'a0947295_carrierordb'                              #Название БД
